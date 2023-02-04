65c43def418d31ec6bef3c63dba0a67cf1ef40bd
zxy <3181127720@qq.com>
2023-02-04 19:25:58 +0800

    小修改

--- models/userFollow.go
+++ models/userFollow.go
@@ -6,9 +6,9 @@ import (
 
 type userFollow struct {
 	gorm.Model
-	UserIdentity   uint64 `gorm:"column:user_identity;type:int;"`   // 用户ID
-	FollowIdentity uint64 `gorm:"column:follow_identity;type:int;"` // 关注者ID
-	Status         string `gorm:"column:status;type:tinyint(1);"`   // 关注状态（0表示未关注，1表示已关注）
+	UserIdentity     uint64 `gorm:"column:user_identity;type:int;"`     // 用户ID
+	FollowerIdentity uint64 `gorm:"column:follower_identity;type:int;"` // 关注者ID
+	Status           string `gorm:"column:status;type:tinyint(1);"`     // 关注状态（0表示未关注，1表示已关注）
 }
 
 func (table *userFollow) TableName() string {
